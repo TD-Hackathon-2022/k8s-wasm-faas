846ac7534312ab0a6d77a9a6f9e54160b8741573
Kai.He <kai.he.0811@qq.com>
2022-01-19 17:59:25 +0800

    fix: fix wrong when build

--- build.sh
+++ build.sh
@@ -6,15 +6,12 @@ TARGET_PORT=${TARGET_PORT}
 TARGET_USER=${TARGET_USER}
 TARGET_PATH=${TARGET_PATH}
 
-chmod 600 /opt/k8s-faas-builder/private-key
-cat /opt/k8s-faas-builder/private-key
-
 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-s -w" -o "$FUNCTION_NAME" "/opt/k8s-faas-builder/lambda/$FUNCTION_NAME"
 
 mkdir ~/.ssh/
 ssh-keyscan -H "$TARGET_HOST" >> ~/.ssh/known_hosts
 
-scp -r -i /opt/k8s-faas-builder/private-key \
+scp -r -i /opt/k8s-faas-builder/config/ssh-privatekey \
   -P "$TARGET_PORT" \
   "$FUNCTION_NAME" \
   "${TARGET_USER}@${TARGET_HOST}:${TARGET_PATH}"
