9b77f18b0f87c638f3fbff8afa78c3cd3c0e8387
Kai.He <kai.he.0811@qq.com>
2022-01-19 18:01:56 +0800

    feat: create when submit faas wasm lambda function

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	batchV1 "k8s.io/api/batch/v1"
 	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	kubeInformers "k8s.io/client-go/informers"
@@ -49,6 +52,7 @@ func main() {
 		AddFunc: func(newConfigMap interface{}) {
 			configmap := newConfigMap.(*coreV1.ConfigMap)
 			fmt.Printf("%s\t%s\n", configmap.UID, configmap.Name)
+			createFaasBuilderJob(clientset, configmap)
 		},
 	})
 
@@ -73,4 +77,90 @@ func setupSignalHandler() (stopCh <-chan struct{}) {
 
 func filterLabel(listOption *v1.ListOptions) {
 	listOption.LabelSelector = labels.FormatLabels(faasLabels)
+}
+
+func createFaasBuilderJob(clientset *kubernetes.Clientset, lambda *coreV1.ConfigMap) {
+	var (
+		ttLSecondsAfterFinished int32 = 100
+		defaultMode             int32 = 0600
+		targetHost                    = os.Getenv("TARGET_HOST")
+		targetPort                    = os.Getenv("TARGET_PORT")
+		targetUser                    = os.Getenv("TARGET_USER")
+		targetPath                    = os.Getenv("TARGET_PATH")
+	)
+
+	_, err := clientset.BatchV1().Jobs(coreV1.NamespaceDefault).Create(
+		context.TODO(),
+		&batchV1.Job{
+			TypeMeta: metaV1.TypeMeta{
+				APIVersion: "v1",
+				Kind:       "Jobs",
+			},
+			ObjectMeta: metaV1.ObjectMeta{
+				Name:      "faas-wasm-builder" + lambda.Name + "-" + string(lambda.UID),
+				Namespace: coreV1.NamespaceDefault,
+				Labels:    faasLabels,
+			},
+			Spec: batchV1.JobSpec{
+				TTLSecondsAfterFinished: &ttLSecondsAfterFinished,
+				Template: coreV1.PodTemplateSpec{
+					Spec: coreV1.PodSpec{
+						NodeSelector: map[string]string{
+							"faas-wasm-runtime": "wasm",
+						},
+						Volumes: []coreV1.Volume{
+							{
+								Name: "faas-builder-ssh-private-key",
+								VolumeSource: coreV1.VolumeSource{
+									Secret: &coreV1.SecretVolumeSource{
+										SecretName:  "faas-builder-ssh-private-key",
+										DefaultMode: &defaultMode,
+									},
+								},
+							},
+							{
+								Name: "lambda",
+								VolumeSource: coreV1.VolumeSource{
+									ConfigMap: &coreV1.ConfigMapVolumeSource{
+										LocalObjectReference: coreV1.LocalObjectReference{Name: lambda.Name},
+									},
+								},
+							},
+						},
+						Containers: []coreV1.Container{
+							{
+								Name:  "k8s-faas-builder",
+								Image: "redxiiikk/k8s-faas-builder:latest",
+								VolumeMounts: []coreV1.VolumeMount{
+									{
+										Name:      "faas-builder-ssh-private-key",
+										ReadOnly:  false,
+										MountPath: "/opt/k8s-faas-builder/config/",
+									},
+									{
+										Name:      "lambda",
+										ReadOnly:  true,
+										MountPath: "/opt/k8s-faas-builder/lambda",
+									},
+								},
+								Env: []coreV1.EnvVar{
+									{Name: "FUNCTION_NAME", Value: lambda.Name},
+									{Name: "TARGET_HOST", Value: targetHost},
+									{Name: "TARGET_PORT", Value: targetPort},
+									{Name: "TARGET_USER", Value: targetUser},
+									{Name: "TARGET_PATH", Value: targetPath},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		metaV1.CreateOptions{},
+	)
+
+	if err != nil {
+		fmt.Println("create faas wasm builder job error", err)
+		return
+	}
 }
