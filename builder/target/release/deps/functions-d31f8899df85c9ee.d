8227af02f71cfaa8f94ab0c39aefe3c15f3567ff
Kai.He <kai.he.0811@qq.com>
2022-02-07 15:06:14 +0800

    chore: delete unused file & update git ignore file

--- builder/target/release/deps/functions-d31f8899df85c9ee.d
+++
@@ -1,6 +0,0 @@
-/Users/kai.he/Workspaces/code/redxiiikk/hackathon-2022/repo/k8s-faas-builder/target/release/deps/functions-d31f8899df85c9ee: src/main.rs src/lambda.rs
-
-/Users/kai.he/Workspaces/code/redxiiikk/hackathon-2022/repo/k8s-faas-builder/target/release/deps/functions-d31f8899df85c9ee.d: src/main.rs src/lambda.rs
-
-src/main.rs:
-src/lambda.rs:
--- builder/target/release/functions.d
+++
@@ -1,1 +0,0 @@
-/Users/kai.he/Workspaces/code/redxiiikk/hackathon-2022/repo/k8s-faas-builder/target/release/functions: /Users/kai.he/Workspaces/code/redxiiikk/hackathon-2022/repo/k8s-faas-builder/src/lambda.rs /Users/kai.he/Workspaces/code/redxiiikk/hackathon-2022/repo/k8s-faas-builder/src/main.rs
